2da8a2ab9875a120cfa43fd9ac5715a3033d64a1
yuanqi0000000 <3331039137@qq.com>
2023-03-13 16:20:44 +0800

    test

--- src/main/java/prog2/exercise4/flight/booking/system/Main.java
+++ src/main/java/prog2/exercise4/flight/booking/system/Main.java
@@ -1,14 +1,93 @@
 package prog2.exercise4.flight.booking.system;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+import java.time.LocalDate;
+
 public class Main 
 {
     public static void main( String[] args )
     {
         
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter your full name:");
+
+        String passengerFullName = input.next();
+
+        System.out.println("Please enter your departure date:");
+
+        String departureDate = input.next();
+
+        System.out.println("Please enter your return date:");
+
+        String returnDate = input.next();
+
+        System.out.println("Please enter number of children:");
+
+        int childPassengers = input.nextInt();
+
+        System.out.println("Please enter number of adults:");
+
+        int adultPassengers = input.nextInt(); 
+       
+        System.out.println("1. First\n2. Bussiness\n3. Economy\nPlease choose your booking class(input the number):");
+
+        String numOfBookingClass = input.next();
+
+        System.out.println("1. One way\n2. Return\nPlease choose your trip type(input the number):");
+
+        String numOfTripType = input.next();
+
+        System.out.println("1. Nanjing\n2. Beijing\n3. Oulu\n4.Helsinki\nPlease choose your trip source(input the number):");
+
+        String numOfTripSource = input.next();
+
+        System.out.println("1. Nanjing\n2. Beijing\n3. Oulu\n4.Helsinki\nPlease choose your trip destination(input the number):");
+
+        String numOfTripDestination = input.next();
+
+        LocalDate departureDate1 = LocalDate.parse(departureDate);
+
+        LocalDate returnDate1 = LocalDate.parse(returnDate);
+        
+        FlightBooking flightBooking1 = new FlightBooking(passengerFullName,departureDate1,returnDate1,childPassengers,adultPassengers);
+
+        flightBooking1.setTotalTicketPrice();
+        
+        flightBooking1.setTotalPassengers(childPassengers,adultPassengers);
+
+        flightBooking1.setBookingClass(numOfBookingClass);
+
+        flightBooking1.setTripType(numOfTripType);
+
+        flightBooking1.setTripSource(numOfTripSource);
+
+        flightBooking1.setTripDestination(numOfTripSource,numOfTripDestination);
+        
+        flightBooking1.setTicketNumber();       
+
+        flightBooking1.setDepartureDate(departureDate1);
+
+        flightBooking1.setReturnDate(returnDate1);
+
+        flightBooking1.setDepartingTicketPrice(childPassengers, adultPassengers);
+
+        flightBooking1.setReturnTicketPrice();
+
+        flightBooking1.setTotalTicketPrice();
+
+      
+        
+
+        
+
+        
+        
+        flightBooking1.setFlightID();
+        
+        input.close();
+
+      
         
     }
 }
